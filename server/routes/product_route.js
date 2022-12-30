04e0cf8b54426c8e4ccaa4d04eef7ff8e90d9648
henrychen79 <s1990050479@gmail.com>
2022-12-30 15:09:33 +0800

    [新增]新增API錯誤處理

--- server/routes/product_route.js
+++ server/routes/product_route.js
@@ -17,25 +17,22 @@ router.get("/testAPI", (req, res) => {
 
 //連接主頁時的API
 router.get("/", (req, res) => {
-<<<<<<< Updated upstream
   let category_id = req.query.category_id;
   let product_num = req.query.product_num;
-  product_M.getSpecificiProduct(category_id, product_num).then((data) => {
-    return res.json(data);
-  });
-=======
-  console.log(product_M.getProduct(1));
-  return res.json(msgObj);
->>>>>>> Stashed changes
+  product_M
+    .getSpecificiProduct(category_id, product_num)
+    .then((data) => res.json(data))
+    .catch((err) => res.status(500).send(err));
 });
 
 //按下頁數後，收到類型跟頁數，回應六筆資料的API
 router.get("/category/:category_id", (req, res) => {
   let page = req.query.page;
   let category_id = req.params.category_id;
-  product_M.getProduct(category_id, page).then((data) => {
-    return res.json(data);
-  });
+  product_M
+    .getProduct(category_id, page)
+    .then((data) => res.json(data))
+    .catch((err) => res.status(500).send(err));
 });
 
 module.exports = router;
