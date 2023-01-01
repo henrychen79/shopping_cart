91f9508e372b3c542f26412f43c4279de514bb7e
henrychen79 <s1990050479@gmail.com>
2023-01-01 14:12:47 +0800

    [新增] express錯誤處理

--- server/index.js
+++ server/index.js
@@ -2,7 +2,6 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const bodyParser = require("body-parser");
-require("./models").initDatabase();
 app.use(cors());
 // 解析 application/x-www-form-urlencoded
 app.use(
@@ -15,6 +14,14 @@ app.use(bodyParser.json());
 
 // 設置路由
 require("./routes")(app);
+
+// 設置錯誤處理
+app.use((err, req, res, next) => {
+  if (err) {
+    console.log("500 error: ", err);
+    return res.status(500).json({ message: err.message });
+  }
+});
 app.listen(8080, () => {
   console.log("Server running on port 8080.");
 });
