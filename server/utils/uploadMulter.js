1c9683156d0151759b4496f270e12e254ec0786f
henrychen79 <s1990050479@gmail.com>
2023-01-02 01:52:47 +0800

    [新增] 使用Multer處理上傳圖片(uploadMulter.js)

---
+++ server/utils/uploadMulter.js
@@ -0,0 +1,21 @@
+const multer = require("multer");
+var fs = require("fs");
+
+var dir = "./public/images";
+if (!fs.existsSync(dir)) {
+  fs.mkdirSync(dir, { recursive: true });
+}
+
+const fileStorageEngine = multer.diskStorage({
+  destination: (req, file, cb) => {
+    console.log(file);
+    cb(null, dir);
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + "-" + file.originalname);
+  },
+});
+
+const upload = multer({ storage: fileStorageEngine });
+
+module.exports = upload;
