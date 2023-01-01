c544d5136157ade41249a5a4f80742820ff5ea65
henrychen79 <s1990050479@gmail.com>
2023-01-01 14:11:45 +0800

    [新增] 商品相關邏輯控制器 (productController.js)

---
+++ server/controllers/productController.js
@@ -0,0 +1,36 @@
+const product_M = require("../models/product");
+const productController = {
+  getProducts: async (req, res, next) => {
+    try {
+      let category_id = req.params.category_id;
+      let page = req.query.page;
+      product_M.getProduct(category_id, page).then((data) => {
+        return res.json(data);
+      });
+    } catch (e) {
+      return next(e);
+    }
+  },
+  allCategory: async (req, res, next) => {
+    try {
+      product_M.product_amount().then((data) => {
+        return res.json(data);
+      });
+    } catch (e) {
+      return next(e);
+    }
+  },
+  getSpecificiProduct: async (req, res, next) => {
+    try {
+      let category_id = req.query.category_id;
+      let product_num = req.query.product_num;
+      product_M.getSpecificiProduct(category_id, product_num).then((data) => {
+        return res.json(data);
+      });
+    } catch (e) {
+      return next(e);
+    }
+  },
+};
+
+module.exports = productController;
--- server/routes/modules/product.js
+++ server/routes/modules/product.js
@@ -1,50 +1,18 @@
 const router = require("express").Router();
-const product_M = require("../../models/product");
+const productController = require("../../controllers/productController");
 
 router.use((req, res, next) => {
-  console.log("A request is coming in to product_route.js");
+  console.log("A request is coming in to product route.js");
   next();
 });
 
-/**********************************以下為假資料測試用******************************************/
-router.get("/testAPI", (req, res) => {
-  const msgObj = {
-    message: "Test API is working.",
-  };
-  return res.json(msgObj);
-});
-/**********************************以上為假資料**********************************************/
+//取得特定商品詳細資訊
+router.get("/", productController.getSpecificiProduct);
 
-//進入商品頁面的API
-router.get("/", (req, res) => {
-  try {
-    let category_id = req.query.category_id;
-    let product_num = req.query.product_num;
-    product_M.getSpecificiProduct(category_id, product_num).then((data) => {
-      return res.json(data);
-    });
-  } catch (error) {
-    console.log("/ ERR" + error);
-    return res.status(404);
-  }
-});
+//取得全部商品類型及其總量
+router.get("/allCategory", productController.allCategory);
 
-//取得商品類型和總量（測試OK）
-router.get("/allCategory", (req, res) => {
-  product_M.product_amount().then((data) => {
-    return res.json(data);
-  });
-});
-
-//按下頁數後，收到類型跟頁數，回應六筆資料的API（測試OK）
-router.get("/category/:category_id", (req, res) => {
-  // console.log(req);
-  let category_id = req.params.category_id;
-  let page = req.query.page;
-  console.log(category_id, page);
-  product_M.getProduct(category_id, page).then((data) => {
-    return res.json(data);
-  });
-});
+//取得特定商品總類中所有商品
+router.get("/category/:category_id", productController.getProducts);
 
 module.exports = router;
