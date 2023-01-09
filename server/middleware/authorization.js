fb820963e38790a80d7bd504f3b465fdca0bd254
henrychen79 <s1990050479@gmail.com>
2023-01-09 16:57:20 +0800

    [新增]檢查登錄使用者得權限(checkIsAdmin)

--- server/middleware/authorization.js
+++ server/middleware/authorization.js
@@ -7,10 +7,8 @@ async function checkJWT(req, res, next) {
     // 驗證 Token
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     // 找尋符合用戶帳號名稱的使用者
-    let user = "";
-    const ret = await user_M.check_account(decoded.account);
-    console.log("chekout account", ret);
-    if (ret.status) user = ret.result[0];
+    const user = await user_M.get_user(decoded.account);
+    console.log("chekout account", user);
     // 若沒找到此用戶，丟出錯誤
     if (!user) {
       throw new Error();
@@ -18,11 +16,30 @@ async function checkJWT(req, res, next) {
     // 將 token 存到 req.token 上供後續使用
     req.token = token;
     // 將用戶完整資料存到 req.user 上供後續使用
-    req.user = user;
+    req.user = user[0];
     next();
   } catch (err) {
     console.log("chekout account err", err);
     res.status(401).send({ error: "Please authenticate." });
   }
 }
+async function checkIsAdmin(req, res, next) {
+  try {
+    // 從來自客戶端請求的 header 取得和擷取JWT
+    const user = req.user;
+
+    console.log("checkIsAdmin", user);
+    // 若不是admin 丟出錯誤
+    if (user.role != "admin") {
+      throw new Error();
+    }
+    next();
+  } catch (err) {
+    console.log("checkIsAdmin err", err);
+    res.status(403).send({
+      error: "The client does not have access rights to the content.",
+    });
+  }
+}
 module.exports.checkJWT = checkJWT;
+module.exports.checkIsAdmin = checkIsAdmin;
--- server/models/user.js
+++ server/models/user.js
@@ -16,7 +16,6 @@ async function insertFakeUser(tableColumns, values) {
 }
 function creatFakeData() {
   try {
-    
     for (const key in users) {
       insertFakeUser(table.user.columnName, [
         users[key].role,
@@ -25,13 +24,10 @@ function creatFakeData() {
         users[key].nickname,
       ]);
     }
-  } catch (error) {
-    
-  }
+  } catch (error) {}
 }
 // creatFakeData()
 /**********************************以上為假資料**********************************************/
-
 
 /**********************************以下為串接ＯＫ**********************************************/
 //確認帳號是否已經存在（測試OK）
@@ -40,7 +36,7 @@ async function check_account(accountName) {
   try {
     let target = `SELECT account FROM user WHERE account = '${accountName}'`;
     const [result, fields] = await db.pool.query(target);
-    console.log('result',result);
+    console.log("result", result);
     // return { status: "ok", result: result };
     if (result.length === 0) {
       return { status: "false", result: result };
@@ -50,6 +46,19 @@ async function check_account(accountName) {
   } catch (error) {
     console.log("fail", error);
     return { status: "fail", result: error };
+  }
+}
+
+async function get_user(accountName) {
+  console.log("get_user測試", accountName);
+  try {
+    let target = `SELECT * FROM user WHERE account = '${accountName}'`;
+    const [result, fields] = await db.pool.query(target);
+    console.log("result", result);
+    return result;
+  } catch (error) {
+    console.log("fail", error);
+    return [];
   }
 }
 // check_account("'rec@gmail.com'");//存在回傳[{account:'帳號名稱'}]  不存在回傳[]
@@ -81,7 +90,6 @@ async function login(accountName, password) {
 
 /**********************************以上為串接ＯＫ**********************************************/
 
-
 /**********************************以下後端postman測試ＯＫ，尚未串接成功**********************************************/
 //STEP1：忘記密碼核對帳號，正確回傳帳號（測試過ＯＫ），讓他可以寄送電子信件
 async function forgetPassword(accountName) {
@@ -100,25 +108,25 @@ async function forgetPassword(accountName) {
 //STEP3：若不重複就新增臨時密碼進去DB(測試ＯＫ)
 //STEP4：寄送暫時密碼信件（./utils/sendMail.js）
 async function createTempPassword(accountName) {
-  let tempPassword = `${Math.floor(Math.random()*10)}${Math.floor(
-    Math.random()*10
-  )}${Math.floor(Math.random()*10)}${Math.floor(
-      Math.random()*10
-  )}${Math.floor(Math.random()*10)}${Math.floor(Math.random()*10)}`;
-  
+  let tempPassword = `${Math.floor(Math.random() * 10)}${Math.floor(
+    Math.random() * 10
+  )}${Math.floor(Math.random() * 10)}${Math.floor(
+    Math.random() * 10
+  )}${Math.floor(Math.random() * 10)}${Math.floor(Math.random() * 10)}`;
+
   let target = `SELECT tempPassword FROM tempInfo WHERE tempPassword = '${tempPassword}'`;
   const [result, fields] = await db.pool.query(target);
-  console.log(result);//沒有重複會回傳[]，有重複會會傳重複的臨時密碼
+  console.log(result); //沒有重複會回傳[]，有重複會會傳重複的臨時密碼
   if (result.length !== 0) {
-    createTempPassword(accountName);    
+    createTempPassword(accountName);
   }
   if (result.length === 0) {
-    let time = new Date().toISOString().slice(0, 19).replace('T', ' ');
+    let time = new Date().toISOString().slice(0, 19).replace("T", " ");
     console.log(time);
-    insertTempPassword(`'${accountName}','${tempPassword}','${time}'`);  //沒有重複，新增臨時密碼進入DB
+    insertTempPassword(`'${accountName}','${tempPassword}','${time}'`); //沒有重複，新增臨時密碼進入DB
     return tempPassword;
   }
-};
+}
 // createTempPassword('cindy81121116@gmail.com');//測試用
 async function insertTempPassword(values, resolve, reject) {
   try {
@@ -126,12 +134,12 @@ async function insertTempPassword(values, resolve, reject) {
     console.log(target);
     await db.pool.query(target);
   } catch (error) {
-    console.log('insertTempPassword ERR:' + error);
+    console.log("insertTempPassword ERR:" + error);
   }
-};
+}
 
 //驗證舊(暫時)密碼（測試ＯＫ）
-async function check_password(tableName,accountName,password) {
+async function check_password(tableName, accountName, password) {
   try {
     let target = `SELECT account FROM ${tableName} 
     WHERE account = '${accountName}' AND
@@ -139,21 +147,22 @@ async function check_password(tableName,accountName,password) {
     const [result, feild] = await db.pool.query(target);
     console.log(result);
     if (result.length === 0) {
-      return false
-    }return true
+      return false;
+    }
+    return true;
   } catch (error) {
-    console.log('check_password ERR:' + error);
+    console.log("check_password ERR:" + error);
   }
-};
+}
 // check_password('tempInfo','cindy81121116@gmail.com','319806');//驗證暫時密碼（測試ＯＫ）
 // check_password('user','rec27@gmail.com','rrr9876')//修改密碼時驗證舊密碼（測試ＯＫ）
 
 //如果舊（暫時）密碼驗證成功，更新新密碼至會員表單DB(測試ＯＫ)
-async function update_password(accountName,newPassword) {
+async function update_password(accountName, newPassword) {
   try {
     let target = `UPDATE user SET password = '${newPassword}' WHERE account = '${accountName}'`;
     await db.pool.query(target);
-    return "update password success";//更新密碼成功
+    return "update password success"; //更新密碼成功
   } catch (error) {
     console.log("updatePassword ERR: " + error);
   }
@@ -161,15 +170,13 @@ async function update_password(accountName,newPassword) {
 // update_password('rec27@gmail.com','rrr1234');//測試用
 /**********************************以上後端postman測試ＯＫ，尚未串接成功**********************************************/
 
-
-
 /**********************************以下尚未完成**********************************************/
 //時間到刪除臨時密碼（測試ＯＫ）尚未export
 async function dropTempPassword(accountName) {
   try {
     let target = `DELETE FROM tempInfo WHERE account = '${accountName}'`;
     await db.pool.query(target);
-    return "tempPassword expired";//臨時密碼過期了
+    return "tempPassword expired"; //臨時密碼過期了
   } catch (error) {
     console.log("dropTempPassword ERR:" + error);
   }
@@ -179,7 +186,6 @@ async function dropTempPassword(accountName) {
 //時間內不能重複寄送信件，時間到過期（@Henry~~~~~這個給你）
 /**********************************以上尚未完成**********************************************/
 
-
 module.exports.login = login;
 module.exports.register = register;
 module.exports.check_account = check_account;
@@ -188,3 +194,4 @@ module.exports.check_password = check_password;
 module.exports.update_password = update_password;
 module.exports.creatFakeData = creatFakeData;
 module.exports.forgetPassword = forgetPassword;
+module.exports.get_user = get_user;
