adb5d616245de0b2994181ab968602f807fda19c
henrychen79 <s1990050479@gmail.com>
2023-01-10 10:59:04 +0800

    [新增]訂單email格式

--- server/middleware/sendMail.js
+++ server/middleware/sendMail.js
@@ -15,7 +15,7 @@ function sendMail(email, subject, html) {
     subject,
     html,
   };
-  console.log(mailOptions);
+  //console.log(mailOptions);
   transporter.sendMail(mailOptions, (err, info) => {
     if (err) {
       console.log(err);
@@ -31,4 +31,52 @@ function forgetPasswordMail(captcha) {
     <h1>${captcha}</h1>
   `;
 }
-module.exports = { sendMail, forgetPasswordMail };
+
+function orderMail(order, status, msg) {
+  return `
+    <html>
+      <head>
+        <style>
+        td { padding: 5px; }
+        .title { background-color: #dcffff; }
+        </style>
+      </head>
+      <body>
+        <div>
+          <p>親愛的顧客，您好:</p>
+          <p>您在射射購物下了一筆訂單，以下為您的訂單資訊 :</p>
+          <table border="3">
+            <tbody>
+            <tr>
+              <td class="title">訂單號碼</td>
+              <td>${order.order_number}</td>
+            </tr>
+            <tr>
+              <td class="title">訂單金額</td>
+              <td>${order.order_prize}</td>
+            </tr>
+            <tr>
+              <td class="title">姓名</td>
+              <td>${order.recipient}</td>
+            </tr>
+            <tr>
+              <td class="title">寄送地址</td>
+              <td>${order.address}</td>
+            </tr>
+            <tr>
+              <td class="title">電話</td>
+              <td>${order.phone}</td>
+            </tr>
+            <tr>
+              <td class="title">訂單狀態</td>
+              <td>${status}</td>
+            </tr>
+          </tbody>
+          </table>
+          <p>${msg}</p>
+        </div>
+      </body>
+    </html>
+  `;
+}
+module.exports = { sendMail, forgetPasswordMail, orderMail };
