6c48f61c0472929fb30fa66712500574babb2675
henrychen79 <s1990050479@gmail.com>
2023-01-11 10:32:11 +0800

    [修改]修正編譯後無法正常顯示畫面問題

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,71 +1,74 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-import ProductView from '../views/ProductView.vue'
-import shoppingCart from '../views/shoppingCart.vue'
+import { createRouter, createWebHashHistory } from "vue-router";
+import HomeView from "../views/HomeView.vue";
+import ProductView from "../views/ProductView.vue";
+import shoppingCart from "../views/shoppingCart.vue";
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: HomeView
+      path: "/",
+      name: "home",
+      component: HomeView,
     },
     {
-      path: '/login',
-      name: 'login',
+      path: "/login",
+      name: "login",
 
-      component: () => import('../views/login.vue')
+      component: () => import("../views/login.vue"),
     },
     {
-      path: '/register',
-      name: 'register',
+      path: "/register",
+      name: "register",
 
-      component: () => import('../views/register.vue')
+      component: () => import("../views/register.vue"),
     },
     {
-      path: '/retrieve',
-      name: 'retrieve',
+      path: "/retrieve",
+      name: "retrieve",
 
-      component: () => import('../views/retrievePassword.vue')
+      component: () => import("../views/retrievePassword.vue"),
     },
     {
-      path: '/member',
-      name: 'member',
-      component: () => import('../views/MemberCentre.vue')
+      path: "/member",
+      name: "member",
+      component: () => import("../views/MemberCentre.vue"),
     },
     {
-      path: '/order',
-      name: 'order',
-      component: () => import('../views/OrderView.vue')
+      path: "/order",
+      name: "order",
+      component: () => import("../views/OrderView.vue"),
     },
     {
-      path: '/:category/:productInfoID',
-      name: 'product',
-      component: ProductView
+      path: "/:category/:productInfoID",
+      name: "product",
+      component: ProductView,
     },
     {
-      path: '/shoppingCart',
+      path: "/shoppingCart",
       component: shoppingCart,
-      children: [{
-        path: '',
-        name: 'shoppingCart',
-        component: () => import('../components/shoppingCart/cartList.vue')
-      }, {
-        path: 'shippingInfo',
-        component: () => import('../components/shoppingCart/shippingInfo.vue')
-      },
-      {
-        path: 'paymentInfo',
-        component: () => import('../components/shoppingCart/paymentInfo.vue')
-      },
-      {
-        path: 'order',
-        component: () => import('../components/shoppingCart/order.vue')
-      }]
-
+      children: [
+        {
+          path: "",
+          name: "shoppingCart",
+          component: () => import("../components/shoppingCart/cartList.vue"),
+        },
+        {
+          path: "shippingInfo",
+          component: () =>
+            import("../components/shoppingCart/shippingInfo.vue"),
+        },
+        {
+          path: "paymentInfo",
+          component: () => import("../components/shoppingCart/paymentInfo.vue"),
+        },
+        {
+          path: "order",
+          component: () => import("../components/shoppingCart/order.vue"),
+        },
+      ],
     },
   ],
-})
+});
 
-export default router
+export default router;
