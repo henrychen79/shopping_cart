90e8e3fa62e4d8a0349474036bc5d19980c95319
henrychen79 <s1990050479@gmail.com>
2023-01-16 17:22:41 +0800

    [新增]會員中心token檢查

--- server/routes/api_route/memberCenter.js
+++ server/routes/api_route/memberCenter.js
@@ -1,11 +1,21 @@
 const router = require("express").Router();
+const authorization = require("../../middleware/authorization");
 const memberCenterController = require("../../controllers/memberCenterController");
 router.use((req, res, next) => {
   console.log("A request is coming in to user route.js");
   next();
 });
 
-router.post("/update_nickname", memberCenterController.update_nickname);
+router.post(
+  "/update_nickname",
+  authorization.checkJWT,
+  memberCenterController.update_nickname
+);
+router.post(
+  "/modify_password",
+  authorization.checkJWT,
+  memberCenterController.modifyPassword
+);
 router.get("/getAllOrders", memberCenterController.allOrders);
 
 module.exports = router;
