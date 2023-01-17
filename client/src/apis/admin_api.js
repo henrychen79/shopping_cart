b219b2744661e999ba9ea72f93f378686888bea1
henrychen79 <s1990050479@gmail.com>
2023-01-17 17:36:47 +0800

    [新增]管理者編輯API

---
+++ client/src/apis/admin_api.js
@@ -0,0 +1,45 @@
+import { send_api, upload_image_api } from "../utils/fetchAPI_base";
+const getProducts = async (fetchCategory1) => {
+  return await send_api(`/admin/getProducts?category=${fetchCategory1}`, "GET");
+};
+const getProduct = async (fetchCategory2, fetchProductNum) => {
+  return await send_api(
+    `/admin/getProduct?category=${fetchCategory2}&productNum=${fetchProductNum}`,
+    "GET"
+  );
+};
+const uploadImage = async (file, category, productNum) => {
+  return await upload_image_api(
+    `/admin/uploadImage?category_id=${category}&product_id=${productNum}`,
+    file
+  );
+};
+const edit_product = async (product_id) => {
+  return await send_api(`/admin/edit_product?product_id=${product_id}`, "GET");
+};
+const update_product = async (body_data) => {
+  return await send_api("/admin/update_product", "POST", body_data);
+};
+const update_price = async (product_id, body_data) => {
+  return await send_api(
+    `/admin/update_price?product_id=${product_id}`,
+    "POST",
+    body_data
+  );
+};
+const update_inventory = async (product_id, body_data) => {
+  return await send_api(
+    `/admin/update_inventory?product_id=${product_id}`,
+    "POST",
+    body_data
+  );
+};
+export {
+  getProducts,
+  getProduct,
+  uploadImage,
+  edit_product,
+  update_product,
+  update_price,
+  update_inventory,
+};
--- client/src/stores/editProductStore.js
+++ client/src/stores/editProductStore.js
@@ -1,150 +1,123 @@
-import { defineStore } from 'pinia'
-import{ref,reactive,computed} from "vue"
-import router from '../router/index'
-import axios from 'axios'
+import { defineStore } from "pinia";
+import { ref, reactive, computed } from "vue";
+import router from "../router/index";
+import axios from "axios";
+import {
+  uploadImage,
+  edit_product,
+  update_product,
+  update_price,
+  update_inventory,
+} from "../apis/admin_api";
 
+export const editProductStore = defineStore("editProductStore", () => {
+  let file = ref("");
+  const dealfilechange = (e) => {
+    file = e.target.files[0];
+  };
 
-export const editProductStore = defineStore('editProductStore',()=>{
+  console.log(router);
+  //存取admin輸入的資料
+  let data = reactive({
+    category: "",
+    productNum: "",
+    productName: "",
+    thumbnail: "",
+    price: "",
+    inventory: "",
+    detail: "",
+    img: "",
+  });
 
-    let file = ref('')
-    const dealfilechange = (e) => {
-        file = e.target.files[0];
-      }
+  const uploadImg = () => {
+    // let param = new FormData();
+    // param.append("image", file);
+    // console.log(param.get("image"));
+    // let config = { headers: { "Content-Type": "multipart/form-data" } };
+    // axios
+    //   .post(
+    //     `http://172.20.10.4:8080/api/admin/uploadImage?category_id=${data.category}&product_id=${data.productNum}`,
+    //     param,
+    //     config
+    //   )
+    //   .then((response) => {
+    //     console.log(response.data);
+    //   });
+    uploadImage(file, data.category, data.productNum).then((ret) => {
+      console.log("uploadImage", ret);
+    });
+  };
 
-    console.log(router)
-    //存取admin輸入的資料
-    let data=reactive({
-        category:'',
-        productNum:'',
-        productName:'',
-        thumbnail:'',
-        price:'',
-        inventory:'',
-        detail:'',
-        img:''
-    })
+  //點擊按鈕
+  const edit = (product_id) => {
+    console.log(data);
+    const url = `http://172.20.10.4:8080/api/admin/edit_product?product_id=${product_id}`;
+    edit_product(product_id)
+      .then(function (res) {
+        console.log("Success:", res);
+        (data.category = res[0].category),
+          (data.productNum = res[0].productNum),
+          (data.productName = res[0].productName),
+          (data.thumbnail = res[0].thumbnail),
+          (data.price = res[0].price),
+          (data.inventory = res[0].inventory),
+          (data.detail = res[0].detail),
+          (data.img = "");
 
-    const uploadImg=()=>{
-        let param = new FormData();
-        param.append("image", file);
-        console.log(param.get('image'));
-        let config = { headers: { "Content-Type": "multipart/form-data" } }
-        axios.post(`http://172.20.10.4:8080/api/admin/uploadImage?category_id=${data.category}&product_id=${data.productNum}`,param,config)
-                .then(response=>{
-                  console.log(response.data);
-                })
-    }
+        router.push({
+          // path:'/admin/editproduct',
+          name: "editproduct",
+        });
+      })
+      .catch(function (error) {
+        console.error("Error:", error);
+      });
+  };
 
+  const update = () => {
+    console.log(data);
+    const url = `http://172.20.10.4:8080/api/admin/update_product`;
+    update_product(JSON.stringify(data))
+      .then(function (res) {
+        console.log("Success:", res);
+      })
+      .catch(function (error) {
+        console.error("Error:", error);
+      });
+  };
 
+  const updatePrice = (product_id) => {
+    // console.log(data)
+    const url = `http://172.20.10.4:8080/api/admin/update_price?product_id=${product_id}`;
+    console.log(data.price);
+    update_price(product_id, JSON.stringify({ price: data.price }))
+      .then(function (res) {
+        console.log("Success:", res);
+      })
+      .catch(function (error) {
+        console.error("Error:", error);
+      });
+  };
 
+  const updateInventory = (product_id) => {
+    console.log(data);
+    const url = `http://172.20.10.4:8080/api/admin/update_inventory?product_id=${product_id}`;
+    update_inventory(product_id, JSON.stringify({ inventory: data.inventory }))
+      .then(function (res) {
+        console.log("Success:", res);
+      })
+      .catch(function (error) {
+        console.error("Error:", error);
+      });
+  };
 
-    //點擊按鈕
-    const edit=(product_id)=>{
-        console.log(data)
-        const url =`http://172.20.10.4:8080/api/admin/edit_product?product_id=${product_id}`
-        fetch(url, {
-            method: 'GET', // or 'PUT'
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            })
-            .then(function(res){
-                console.log(res)
-                return res.json()
-            })
-            .then(function(res){
-                console.log('Success:', res);
-                data.category=res[0].category,
-                data.productNum=res[0].productNum,
-                data.productName=res[0].productName,
-                data.thumbnail=res[0].thumbnail,
-                data.price=res[0].price,
-                data.inventory=res[0].inventory,
-                data.detail=res[0].detail,
-                data.img=''
-
-                router.push({
-                    // path:'/admin/editproduct',
-                    name:'editproduct',
-                })
-                
-            })
-            .catch(function(error){
-                console.error('Error:', error);
-            });
-    }
-
-    const update=()=>{
-        console.log(data)
-        const url =`http://172.20.10.4:8080/api/admin/update_product`
-        fetch(url, {
-            method: 'POST', // or 'PUT'
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(data),
-            })
-            .then(function(res){
-                console.log(res)
-                return res.json()
-            })
-            .then(function(res){
-                console.log('Success:', res);
-            })
-            .catch(function(error){
-                console.error('Error:', error);
-            });
-    }
-
-    const updatePrice=(product_id)=>{
-        // console.log(data)
-        const url =`http://172.20.10.4:8080/api/admin/update_price?product_id=${product_id}`
-        console.log(data.price);
-        fetch(url, {
-            method: 'POST', // or 'PUT'
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({price:data.price}),
-            })
-            .then(function(res){
-                console.log(res)
-                return res.json()
-            })
-            .then(function(res){
-                console.log('Success:', res);
-            })
-            .catch(function(error){
-                console.error('Error:', error);
-            });
-    }
-
-    const updateInventory=(product_id)=>{
-        console.log(data)
-        const url =`http://172.20.10.4:8080/api/admin/update_inventory?product_id=${product_id}`
-        fetch(url, {
-            method: 'POST', // or 'PUT'
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({inventory:data.inventory}),
-            })
-            .then(function(res){
-                console.log(res)
-                return res.json()
-            })
-            .then(function(res){
-                console.log('Success:', res);
-            })
-            .catch(function(error){
-                console.error('Error:', error);
-            });
-    }
-
-
-
-    return{
-        data,
-        edit,update,updatePrice,updateInventory,uploadImg,dealfilechange
-    }
-})
+  return {
+    data,
+    edit,
+    update,
+    updatePrice,
+    updateInventory,
+    uploadImg,
+    dealfilechange,
+  };
+});
